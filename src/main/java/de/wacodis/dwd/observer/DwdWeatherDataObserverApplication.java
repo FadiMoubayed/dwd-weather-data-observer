19a6fab64d5cfc820e7ce8f3a54fddc8c3012fe4
FadiMoubayed <48293530+FadiMoubayed@users.noreply.github.com>
2020-04-30 11:48:46 +0200

    initial commit

---
+++ src/main/java/de/wacodis/dwd/observer/DwdWeatherDataObserverApplication.java
@@ -0,0 +1,13 @@
+package de.wacodis.dwd.observer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwdWeatherDataObserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwdWeatherDataObserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/wacodis/dwd/observer/DwdWeatherDataObserverApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wacodis.dwd.observer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwdWeatherDataObserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
